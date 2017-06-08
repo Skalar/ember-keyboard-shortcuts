050994f78a81c8dd770672c4e870056d3d1294ee
Timmy O'Mahony <hey@timmyomahony.com>
2017-06-08 22:50:34 +0100

    Added Fastboot 1.0 compatibility

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+var map = require('broccoli-stew').map;
+
 module.exports = {
   name: 'ember-keyboard-shortcuts',
+  treeForVendor(vendorTree) {
+    var mouseTrap = new Funnel(path.dirname(require.resolve('mousetrap/mousetrap.js')), {
+      files: ['mousetrap.js'],
+    });
+    mouseTrap = map(mouseTrap, (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
+
+    var trees = [];
+  
+    if (vendorTree !== undefined) {
+      trees.push(vendorTree);
+    }
+    
+    trees.push(mouseTrap);
+    
+    return new MergeTrees(trees);
+  },
 
   included: function(app, parentAddon) {
-    var target = (parentAddon || app);
-    target.import(app.bowerDirectory + '/mousetrap/mousetrap.js');
+    app.import('vendor/mousetrap.js');
   }
 };
