a92658bae0c991d8820be9ae92bbe635e79271d3
Andrew Fan <andrew.fan@upsilonit.com>
2017-07-27 11:48:55 +0300

    add support for engines and fastboot 1.0+

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 /* jshint node: true */
 'use strict';
+var path = require('path');
+var resolve = require('resolve');
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var fastbootTransform = require('fastboot-transform');
 
 module.exports = {
   name: 'ember-keyboard-shortcuts',
 
-  included: function(app, parentAddon) {
-    var target = (parentAddon || app);
-    target.import(app.bowerDirectory + '/mousetrap/mousetrap.js');
-  }
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+    app.import('vendor/mousetrap/mousetrap.js');
+  },
+
+  treeForVendor: function(tree) {
+    var trees = [];
+    var mousetrap = fastbootTransform(new Funnel(this.pathBase('mousetrap'), {
+      destDir: 'mousetrap'
+    }));
+    trees = trees.concat([mousetrap]);
+    if (tree) {
+      trees.push(tree);
+    }
+    return mergeTrees(trees);
+  },
+
+  pathBase: function(packageName) {
+    return path.dirname(resolve.sync(packageName + '/package.json', { basedir: __dirname }));
+  },
 };
