6020ec138fe9402e623d8520b28275c79b2ce1d7
Josemar Luedke <josemarluedke@gmail.com>
2018-07-24 11:35:14 -0700

    Use new Ember imports in create-mixin

--- addon/create-mixin.js
+++ addon/create-mixin.js
@@ -1,49 +1,51 @@
 /* global Mousetrap */
-
-import Ember from 'ember';
+import Mixin from '@ember/object/mixin';
+import { on } from '@ember/object/evented';
+import { typeOf } from '@ember/utils';
 
 export default function(bindEvent, unbindEvent) {
-
-  return Ember.Mixin.create({
-    mousetraps:[],
-    bindShortcuts: Ember.on(bindEvent, function() {
+  return Mixin.create({
+    mousetraps: [],
+    bindShortcuts: on(bindEvent, function() {
       const shortcuts = this.get('keyboardShortcuts');
       const self = this;
-      if (Ember.typeOf(shortcuts) !== 'object') { return; }
+      if (typeOf(shortcuts) !== 'object') {
+        return;
+      }
 
       this.mousetraps = [];
 
       Object.keys(shortcuts).forEach(function(shortcut) {
-        const actionObject   = shortcuts[shortcut];
+        const actionObject = shortcuts[shortcut];
         let mousetrap;
         let preventDefault = true;
 
         function invokeAction(action, eventType) {
-          var type = Ember.typeOf(action);
+          var type = typeOf(action);
           var callback;
           if (type === 'string') {
-            callback = function(){
+            callback = function() {
               self.send(action);
               return preventDefault !== true;
-            }
-          }
-          else if (type === 'function') {
-            callback = action.bind(self)
-          }
-          else {
+            };
+          } else if (type === 'function') {
+            callback = action.bind(self);
+          } else {
             throw new Error('Invalid value for keyboard shortcut: ' + action);
           }
           mousetrap.bind(shortcut, callback, eventType);
         }
 
-        if (Ember.typeOf(actionObject) === 'object') {
+        if (typeOf(actionObject) === 'object') {
           if (actionObject.global === false) {
             mousetrap = new Mousetrap(document);
           } else if (actionObject.scoped) {
-            if (Ember.typeOf(actionObject.scoped) === 'boolean') {
+            if (typeOf(actionObject.scoped) === 'boolean') {
               mousetrap = new Mousetrap(this.get('element'));
-            } else if (Ember.typeOf(actionObject.scoped) === 'string') {
-              mousetrap = new Mousetrap(document.querySelector(actionObject.scoped));
+            } else if (typeOf(actionObject.scoped) === 'string') {
+              mousetrap = new Mousetrap(
+                document.querySelector(actionObject.scoped)
+              );
             }
           } else if (actionObject.targetElement) {
             mousetrap = new Mousetrap(actionObject.targetElement);
@@ -62,30 +64,24 @@ export default function(bindEvent, unbindEvent) {
         }
         self.mousetraps.push(mousetrap);
       }, this);
-
     }),
 
-    unbindShortcuts: Ember.on(unbindEvent, function() {
+    unbindShortcuts: on(unbindEvent, function() {
       const _removeEvent = (object, type, callback) => {
         if (object.removeEventListener) {
           object.removeEventListener(type, callback, false);
           return;
         }
         object.detachEvent('on' + type, callback);
-      }
-      this.mousetraps.forEach(
-        (mousetrap) => {
-          // manually unbind JS event
-          _removeEvent(mousetrap.target, 'keypress', mousetrap._handleKeyEvent);
-          _removeEvent(mousetrap.target, 'keydown', mousetrap._handleKeyEvent);
-          _removeEvent(mousetrap.target, 'keyup', mousetrap._handleKeyEvent);
-          mousetrap.reset()
-        }
-      );
-      this.mousetraps = []
+      };
+      this.mousetraps.forEach(mousetrap => {
+        // manually unbind JS event
+        _removeEvent(mousetrap.target, 'keypress', mousetrap._handleKeyEvent);
+        _removeEvent(mousetrap.target, 'keydown', mousetrap._handleKeyEvent);
+        _removeEvent(mousetrap.target, 'keyup', mousetrap._handleKeyEvent);
+        mousetrap.reset();
+      });
+      this.mousetraps = [];
     })
-
   });
-
-
 }
